47b7ccc5aa0af91f26786c49d5f62fbfc228b501
sacigazmar <gsara.gazmar@gmail.com>
2023-10-20 19:25:23 +0200

    modified:   assets/css/style.css 	modified:   assets/css/style2.css 	new file:   assets/images/background oval black.png 	deleted:    assets/images/background.JPG 	new file:   assets/images/chill.JPG 	new file:   assets/images/fairies.JPG 	new file:   assets/images/lotus.gif 	new file:   assets/images/philips.gif 	new file:   assets/images/plate2.png 	new file:   assets/images/plate3.png 	modified:   assets/js/index.js 	modified:   index.html 	modified:   index2.html 	modified:   assets/css/style.css 	modified:   assets/css/style2.css     deleted:    assets/images/CLUBLOVE.png 	deleted:    assets/images/plate.png 	deleted:    assets/images/plate2.png

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,9 @@
-const helloWorld = () => {
-  console.log("Hello World");
-};
+const minimapIndicator = document.querySelector( '#minimap > div' );
 
-helloWorld();
+document.addEventListener( 'scroll', ( ) => {
+  const percX = window.pageXOffset / ( window.innerWidth * 5 );
+  const percY = window.pageYOffset / ( window.innerHeight * 5 );
+
+  minimapIndicator.style.left = ( 100 * percX ) + '%';
+  minimapIndicator.style.top = ( 100 * percY ) + '%';
+} );
